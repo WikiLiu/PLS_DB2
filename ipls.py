a3288eae09179341c03cff928d3249722b48ce48
WikiLiu <1307996390@qq.com>
2023-02-19 20:13:46 +0800

    chat加入 预训练模型

--- ipls.py
+++ ipls.py
@@ -1,63 +1,58 @@
 from sklearn.cross_decomposition import PLSRegression
-
-# 创建PLSRegression对象
-pls = PLSRegression(n_components=2)
-
-# 用大类数据集训练PLS模型
-pls.fit(data_big.iloc[:, :-1], data_big.iloc[:, -1])
-
-# 获取模型的系数和截距
-coefficients = pls.coef_
-intercept = pls.y_mean_ - np.dot(pls.x_mean_, coefficients)
-
-
-# 对小类数据集进行预测
-predictions = np.dot(data_small.iloc[:, :-1], coefficients) + intercept
-
-# 计算预测误差
-errors = data_small.iloc[:, -1] - predictions
-
-# 使用误差对模型参数进行纠偏
-coefficients_corrected = coefficients + np.dot(data_small.iloc[:, :-1].T, errors) / np.linalg.norm(errors)
-intercept_corrected = intercept + np.mean(errors)
-# 使用纠偏后的模型参数对小类数据集进行重新预测
-predictions_corrected = np.dot(data_small.iloc[:, :-1], coefficients_corrected) + intercept_corrected
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''
+import numpy as np
+import pandas as pd
+import numpy as np
+from sklearn.utils import shuffle
+import pandas as pd
+from sklearn import cross_decomposition, model_selection, preprocessing
 from sklearn.cross_decomposition import PLSRegression
+from sklearn.metrics import mean_absolute_error
+from sklearn.cross_decomposition import PLSRegression
+from sklearn.model_selection import KFold
 
-# 创建PLSRegression对象
-pls = PLSRegression(n_components=2)
+df = pd.read_csv("output.csv", index_col=0)
+df = df[[col for col in df.columns if col != 'DELTA_THICK_7'] + ['DELTA_THICK_7']]
+data_small = df.drop(["STAND_NO_6","STAND_NO_7"],axis=1)
+X_small = data_small.drop(["DELTA_THICK_7", "STRIP_NO_6"], axis=1)
+Y_small = data_small["DELTA_THICK_7"]
+x_scaled = preprocessing.scale(X_small)
 
-# 用大类数据集训练PLS模型
-pls.fit(data_big.iloc[:, :-1], data_big.iloc[:, -1])
+df = pd.read_csv("output_pre.csv", index_col=0)
+df = df[[col for col in df.columns if col != 'DELTA_THICK_7'] + ['DELTA_THICK_7']]
+data_big = df.drop(["STAND_NO_6","STAND_NO_7"],axis=1)
+X_big = data_big.drop(["DELTA_THICK_7", "STRIP_NO_6"], axis=1)
+Y_big = data_big["DELTA_THICK_7"]
+# 导入数据并进行均值方差归一化
+X_scaled = preprocessing.scale(X_big)
+# Y_scaled = preprocessing.scale(Y)
+# 打乱顺序
+X_shuffle, Y_shuffle = shuffle(X_scaled, Y_big, random_state=0)
+# 划分训练集和测试集
+X_train, X_test, Y_train, Y_test = model_selection.train_test_split(X_shuffle, Y_shuffle, test_size=0.3, random_state=0)
+# 定义偏最小二乘回归模型
+pls = PLSRegression(scale=True)
+# 交叉验证选择最佳的组分数量
+n_components = np.arange(1, 21)
+scores = []
+for n in n_components:
+    pls.set_params(n_components=n)
+    score = model_selection.cross_val_score(pls, X_train, Y_train, cv=10, scoring='neg_mean_absolute_error')
+    scores.append(-score.mean())
+best_n = n_components[np.argmin(scores)]
+# 使用最佳的组分数量训练模型
+pls.set_params(n_components=best_n)
+pls.fit(X_train, Y_train)
+
+# # 用大类数据集训练PLS模型
+# pls.fit(data_big.iloc[:, :-1], data_big.iloc[:, -1])
 
 # 获取模型的系数和截距
 coefficients = pls.coef_
-intercept = pls.y_mean_ - np.dot(pls.x_mean_, coefficients)
+intercept = pls._y_mean - np.dot(pls._x_mean, coefficients)
+
 # 对小类数据集进行预测
-predictions = np.dot(data_small.iloc[:, :-1], coefficients) + intercept
+predictions = np.dot(x_scaled, coefficients) + intercept
+pre2 = pls.predict(x_scaled)
 # 计算预测误差
 errors = data_small.iloc[:, -1] - predictions
 
@@ -65,43 +60,6 @@ errors = data_small.iloc[:, -1] - predictions
 pls_final = PLSRegression(n_components=2)
 pls_final.fit(data_small.iloc[:, :-1], data_small.iloc[:, -1])
 coefficients_final = pls_final.coef_
-intercept_final = pls_final.y_mean_ - np.dot(pls_final.x_mean_, coefficients_final) + np.mean(errors)
+intercept_final = pls_final._y_mean - np.dot(pls_final._x_mean, coefficients_final) + np.mean(errors)
 # 使用最终的模型参数对数据进行预测
-predictions_final = np.dot(data_test.iloc[:, :-1], coefficients_final) + intercept_final
-from sklearn.cross_decomposition import PLSRegression
-from sklearn.model_selection import KFold
-
-# 将大类数据集分为训练集和验证集
-train_size = 0.8
-n_splits = 5
-kf = KFold(n_splits=n_splits, shuffle=True, random_state=42)
-n_train = int(train_size * len(data_big))
-train_indices = list(kf.split(data_big))[0][0][:n_train]
-X_train = data_big.iloc[train_indices, :-1]
-y_train = data_big.iloc[train_indices, -1]
-X_val = data_big.iloc[:, :-1].drop(train_indices)
-y_val = data_big.iloc[:, -1].drop(train_indices)
-
-# 选择潜变量数量
-n_components_list = [1, 2, 3, 4, 5]
-best_n_components = 0
-best_mse = float('inf')
-for n_components in n_components_list:
-    pls = PLSRegression(n_components=n_components)
-    pls.fit(X_train, y_train)
-    y_pred = pls.predict(X_val)
-    mse = np.mean((y_val - y_pred) ** 2)
-    if mse < best_mse:
-        best_mse = mse
-        best_n_components = n_components
-# 用最优的潜变量数量重新拟合模型
-pls_final = PLSRegression(n_components=best_n_components)
-pls_final.fit(data_big.iloc[:, :-1], data_big.iloc[:, -1])
-
-# 使用小类数据集对模型进行拟合
-coefficients_final = pls_final.coef_
-intercept_final = pls_final.y_mean_ - np.dot(pls_final.x_mean_, coefficients_final)
 predictions_final = np.dot(data_small.iloc[:, :-1], coefficients_final) + intercept_final
-
-
-'''
--- pre_pls.py
+++ pre_pls.py
@@ -2,6 +2,7 @@ import numpy as np
 from sklearn.utils import shuffle
 import pandas as pd
 from sklearn import cross_decomposition, model_selection, preprocessing
+from sklearn.cross_decomposition import PLSRegression
 from sklearn.metrics import mean_absolute_error
 
 
@@ -28,21 +29,68 @@ X_shuffle, Y_shuffle = shuffle(X_scaled, Y, random_state=0)
 # 划分训练集和测试集
 X_train, X_test, Y_train, Y_test = model_selection.train_test_split(X_shuffle, Y_shuffle, test_size=0.3, random_state=0)
 
-# 训练偏最小二乘回归模型
-pls = cross_decomposition.PLSRegression(n_components=25)
+# 定义偏最小二乘回归模型
+pls = PLSRegression(scale=True)
+
+# 交叉验证选择最佳的组分数量
+n_components = np.arange(1, 21)
+scores = []
+for n in n_components:
+    pls.set_params(n_components=n)
+    score = model_selection.cross_val_score(pls, X_train, Y_train, cv=10, scoring='neg_mean_absolute_error')
+    scores.append(-score.mean())
+best_n = n_components[np.argmin(scores)]
+
+# 使用最佳的组分数量训练模型
+pls.set_params(n_components=best_n)
 pls.fit(X_train, Y_train)
 
 # 预测测试集结果
 Y_pred = pls.predict(X_test)
 
 # 计算预测精度
-score = mean_absolute_error(Y_test, Y_pred)
-print('偏最小二乘模型预测精度：', score)
-
-
+score = -mean_absolute_error(Y_test, Y_pred)
+print('偏最小二乘模型预测精度（L1 loss）：', score,best_n)
 
 import matplotlib.pyplot as plt
 plt.scatter( Y_pred, Y_test)
 plt.show()
+
+
+
+
+
+
+
+
+from sklearn.model_selection import GridSearchCV
+from sklearn.cross_decomposition import PLSRegression
+
+# 定义参数范围
+param_grid = {'n_components': [2, 3, 4, 5],
+              'scale': [True, False],
+              'max_iter': [100, 200, 300],
+              'tol': [1e-3, 1e-4, 1e-5],
+              'copy': [True, False]}
+
+# 初始化模型和网格搜索
+pls = PLSRegression()
+grid_search = GridSearchCV(pls, param_grid, cv=5, scoring='neg_mean_squared_error')
+
+# 在训练集上进行网格搜索
+grid_search.fit(X_train, Y_train)
+
+# 打印最佳超参数和最佳交叉验证得分
+print('Best parameters:', grid_search.best_params_)
+print('Best CV score:', -grid_search.best_score_)
+
+# 使用最佳超参数重新训练模型并预测测试集
+best_pls = grid_search.best_estimator_
+Y_pred = best_pls.predict(X_test)
+
+# 计算预测精度
+score = -mean_absolute_error(Y_test, Y_pred)
+print('L1 loss:', score)
+
 
 
